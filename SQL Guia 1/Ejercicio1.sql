e0871c32b3d2fc4f2dc5dd1807e3e23aa491a9fd
Alvaro Arenas <alvaro.arenas973@gmail.com>
2021-06-12 09:46:58 -0400

    Update clase 3

--- SQL Guia 1/Ejercicio1.sql
+++ SQL Guia 1/Ejercicio1.sql
@@ -9,10 +9,10 @@ GRANT CONNECT, RESOURCE TO C##ERROR_EMPLEADO;
 -- User: C##ERROR_EMPLEADO
 
 -- Crear tabla EMPLEADO
-CREATE TABLE EMPLEADO
-( id_emp            NUMBER(7)    NOT NULL CONSTRAINT PK_EMPLEADO PRIMARY KEY,
-  apell_pat         VARCHAR2(25) NOT NULL,
-  primer_nombre     VARCHAR2(20) NOT NULL,
-  salario           NUMBER(8)    NOT NULL,
-  cod_depto         NUMBER(7)    NOT NULL
+CREATE TABLE EMPLEADO( 
+	id_emp            NUMBER(7)    NOT NULL CONSTRAINT PK_EMPLEADO PRIMARY KEY,
+  	apell_pat         VARCHAR2(25) NOT NULL,
+ 	primer_nombre     VARCHAR2(20) NOT NULL,
+  	salario           NUMBER(8)    NOT NULL,
+  	cod_depto         NUMBER(7)    NOT NULL
 );
--- SQL Guia 1/Ejercicio4.sql
+++ SQL Guia 1/Ejercicio4.sql
@@ -9,30 +9,30 @@ GRANT CONNECT, RESOURCE TO C##SABORES;
 -- User: C##SABORES
 
 -- Crear tabla CLIENTE
-CREATE TABLE CLIENTE
-( id_cliente        NUMBER(5)    NOT NULL CONSTRAINT PK_CLIENTE PRIMARY KEY,
-  primer_nombre     VARCHAR2(25) NOT NULL,
-  segundo_nombre    VARCHAR2(25),
-  apell_paterno     VARCHAR2(25) NOT NULL,
-  apell_materno     VARCHAR2(25) NOT NULL,
-  direccion         VARCHAR2(30) NOT NULL
+CREATE TABLE CLIENTE(
+	id_cliente        NUMBER(5)    NOT NULL CONSTRAINT PK_CLIENTE PRIMARY KEY,
+	primer_nombre     VARCHAR2(25) NOT NULL,
+	segundo_nombre    VARCHAR2(25),
+	apell_paterno     VARCHAR2(25) NOT NULL,
+	apell_materno     VARCHAR2(25) NOT NULL,
+	direccion         VARCHAR2(30) NOT NULL
 );
 
 -- Crear tabla EMPLEADO
-CREATE TABLE EMPLEADO
-( id_empleado       NUMBER(6)    NOT NULL CONSTRAINT PK_EMPLEADO PRIMARY KEY,
-  primer_nombre     VARCHAR2(25) NOT NULL,
-  segundo_nombre    VARCHAR2(25),
-  apell_paterno     VARCHAR2(25) NOT NULL,
-  apell_materno     VARCHAR2(25) NOT NULL,
-  fecha_contrato    DATE         NOT NULL
+CREATE TABLE EMPLEADO( 
+	id_empleado       NUMBER(6)    NOT NULL CONSTRAINT PK_EMPLEADO PRIMARY KEY,
+	primer_nombre     VARCHAR2(25) NOT NULL,
+	segundo_nombre    VARCHAR2(25),
+	apell_paterno     VARCHAR2(25) NOT NULL,
+	apell_materno     VARCHAR2(25) NOT NULL,
+	fecha_contrato    DATE         NOT NULL
 );
 
 -- Crear tabla PEDIDO
-CREATE TABLE PEDIDO
-( nro_pedido    NUMBER(6)   NOT NULL    CONSTRAINT PK_PEDIDO    PRIMARY KEY,
-  fecha_pedido  DATE        NOT NULL,
-  fecha_entrega DATE        NOT NULL,
-  id_cliente    NUMBER(5)   NOT NULL    CONSTRAINT FK_PEDIDO_CLIENTE    REFERENCES CLIENTE  (id_cliente),
-  id_empleado   NUMBER(6)   NOT NULL    CONSTRAINT FK_PEDIDO_EMPLEADO   REFERENCES EMPLEADO (id_empleado)
+CREATE TABLE PEDIDO(
+	nro_pedido    NUMBER(6)   NOT NULL    CONSTRAINT PK_PEDIDO    PRIMARY KEY,
+	fecha_pedido  DATE        NOT NULL,
+	fecha_entrega DATE        NOT NULL,
+	id_cliente    NUMBER(5)   NOT NULL    CONSTRAINT FK_PEDIDO_CLIENTE    REFERENCES CLIENTE  (id_cliente),
+	id_empleado   NUMBER(6)   NOT NULL    CONSTRAINT FK_PEDIDO_EMPLEADO   REFERENCES EMPLEADO (id_empleado)
 );
